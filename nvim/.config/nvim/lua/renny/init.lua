1633a1dfeeec173347db80a3858868019a14afcd
Renny <rennyhong1103@gmail.com>
2022-04-12 21:06:11 -0700

    remove scroll

--- nvim/.config/nvim/lua/renny/init.lua
+++ nvim/.config/nvim/lua/renny/init.lua
@@ -2,7 +2,6 @@ require("renny.lsp")
 require("renny.indent_blankline")
 require("renny.comment")
 require("renny.lualine")
-require('renny.scroll')
 
 require'nvim-treesitter.configs'.setup {--[[  highlight = { enable = true } ]]}
 
