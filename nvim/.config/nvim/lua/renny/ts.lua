0b047a5f081bb8f9bad9258dddda9089752b69f4
Renny <rennyhong1103@gmail.com>
2022-04-30 01:15:54 -0700

    changed some colors for treesitter so that the texts will be less distracting

--- nvim/.config/nvim/lua/renny/ts.lua
+++ nvim/.config/nvim/lua/renny/ts.lua
@@ -8,7 +8,7 @@ require'nvim-treesitter.configs'.setup{
 
 -- These are for solarized-dark and C
 require'nvim-treesitter.highlight'.set_custom_captures{
-  ["type"] = "TSType",
+  ["type"] = "TSString",
   ["punctuation.delimiter"] = "TSString",
   ["property"] = "",
   ["parameter"] = "",
