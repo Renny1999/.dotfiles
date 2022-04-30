cf190f322743e54bad20bac9c9dfe302384b85ec
Renny <rennyhong1103@gmail.com>
2022-04-30 09:57:45 -0700

    fixed ts color issue

--- nvim/.config/nvim/lua/renny/ts.lua
+++ nvim/.config/nvim/lua/renny/ts.lua
@@ -9,7 +9,7 @@ require'nvim-treesitter.configs'.setup{
 -- These are for solarized-dark and C
 require'nvim-treesitter.highlight'.set_custom_captures{
   ["type"] = "TSString",
-  ["punctuation.delimiter"] = "TSString",
-  ["property"] = "",
-  ["parameter"] = "",
+  ["punctuation.delimiter"] = "TSOperator",
+  ["property"] = "TSVariable",
+  ["parameter"] = "TSVariable",
 }
