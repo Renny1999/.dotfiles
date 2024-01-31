b6f2ba7014dbd0a7ed87057d8a5e3a58accd0096
Renny1999 <33414341+Renny1999@users.noreply.github.com>
2024-01-31 14:21:47 -0800

    when popping from the tag stack, the buffer will be retained if there are windows with the buffer open

--- nvim/.config/nvim/lua/renny/init.lua
+++ nvim/.config/nvim/lua/renny/init.lua
@@ -12,12 +12,12 @@ require("renny.leap")
 
 
 -- our goal is to delete buffer when going back the stack with ctrl-t
-
 DBG = 1
-function dbg(x)
+function mydbg(x)
   if (DBG == 1) then
     print(x)
   end
+  return
 end
 
 function mypop()
@@ -25,54 +25,72 @@ function mypop()
   local curbufnr = vim.fn.bufnr()
   local tagstack = vim.fn.gettagstack(windowid)
   local prev
+  local prevbufnr
+
+  local res,retval -- used for calling pop
   -- check if the next elem in the tag stack and the current one is the same buffer
 
   local items = tagstack['items']
   local curidx = tagstack['curidx']
-  dbg('curidx ' .. curidx)
-  dbg('length ' .. tagstack['length'])
+  mydbg('curidx ' .. curidx)
+  mydbg('length ' .. tagstack['length'])
 
   if curidx == 1 then
-    dbg('popping due to curidx=1')
+    mydbg('popping due to curidx=1')
     goto pop_one
   end
 
   prev = items[curidx-1]
 
   if prev == nil then
-    dbg('popping due to prev=nil')
+    mydbg('popping due to prev=nil')
     goto pop_one
   end
 
-  dbg('prev buf number ' .. prev['bufnr'])
-  dbg('current buf number ' .. curbufnr)
+  prevbufnr = prev['bufnr']
+
+  mydbg('before pop: prev buf number ' .. prev['bufnr'])
+  mydbg('before pop: current buf number ' .. curbufnr)
+  mydbg('stub')
+
+  -- pop the tag, then delete the buffer
+  res, retval = pcall(pp)
+
+  if res then
+  else
+    local x,y = string.find(retval, 'Vim%(pop%):E555:')
+    if (x ~= nil) then
+      print('E555: at bottom of tag stack')
+    else
+      printerr(retval)
+    end
+  end
+
+  mydbg('after pop: prev buf number ' .. prev['bufnr'])
+  mydbg('after pop: current buf number ' .. curbufnr)
 
   if (prev['bufnr'] ~= curbufnr) then
-    -- close the current buffer
-    if vim.fn.bufexists(curbufnr) then
-      vim.cmd('bdelete' .. curbufnr)
+    -- delete the buffer if it is not opened in any other window
+    local bufwins = vim.fn.win_findbuf(curbufnr)
+    local bufwinnums = table.getn(bufwins)
+    mydbg(bufwinnums .. ' windows with the buffer after pop')
+    if bufwinnums == 0 then
+      if vim.fn.bufexists(curbufnr) then
+        vim.cmd('bdelete' .. curbufnr)
+      end
+    else
+      mydbg('cannot delete buffer ' .. curbufnr .. ' because it is open in ' .. bufwinnums .. ' windows')
     end
   end
 
 ::pop_one::
   return
-  -- local res,retval = pcall(pp)
-  -- if res then
-  -- else
-  --   local x,y = string.find(retval, 'Vim%(pop%):E555:')
-  --   if (x ~= nil) then
-  --     print('E555: at bottom of tag stack')
-  --   else
-  --     printerr(retval)
-  --   end
-  -- end
 
 end
 
-vim.keymap.set('n', '<C-t>', "<cmd>lua require('renny.init') mypop()<CR> :pop<CR>", {noremap=true})
+-- vim.keymap.set('n', '<C-t>', "<cmd>lua require('renny.init') mypop()<CR> :pop<CR>", {noremap=true})
+vim.keymap.set('n', '<C-t>', "<cmd>lua require('renny.init') mypop()<CR>", {noremap=true})
 
 function pp()
   vim.cmd('pop')
 end
-
-
